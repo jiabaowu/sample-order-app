f9d7ea07b114929fb0e46363388f74a73785f01d
ivy <jiabao.literatu@gmail.com>
2018-09-06 01:47:15 -0700

    add statis dir to server

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+// server.js
+const path = require('path')
+const express = require('express')
+const jsonServer = require('json-server')
+// const routes = require('./routes.json')
 const port = process.env.PORT || 3000;
 
-server.use(middlewares);
-server.use(router);
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
 
-server.listen(port);
+server.use('/', express.static(path.join(__dirname, 'dist')))
+
+// server.use(jsonServer.rewriter(routes))
+server.use('/api', router)
+
+server.listen(port, () => {
+  console.log('JSON Server is running');
+})
